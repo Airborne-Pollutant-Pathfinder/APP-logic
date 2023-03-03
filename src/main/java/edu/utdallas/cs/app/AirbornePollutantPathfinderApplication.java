f83caa63a29f4734fed3bf8905519778169feeae
Arham4 <arhamjsiddiqui@gmail.com>
2023-03-02 19:18:40 -0600

    Initial commit

---
+++ src/main/java/edu/utdallas/cs/app/AirbornePollutantPathfinderApplication.java
@@ -0,0 +1,13 @@
+package edu.utdallas.cs.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbornePollutantPathfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirbornePollutantPathfinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/utdallas/cs/app/AirbornePollutantPathfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.utdallas.cs.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbornePollutantPathfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
