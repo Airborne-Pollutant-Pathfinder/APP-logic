e2956891d7239433e9a320114246e770520f9fb7
Arham4 <arhamjsiddiqui@gmail.com>
2023-03-03 19:56:28 -0600

    Apply code reformatting

--- src/main/java/edu/utdallas/cs/app/AirbornePollutantPathfinderApplication.java
+++ src/main/java/edu/utdallas/cs/app/AirbornePollutantPathfinderApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AirbornePollutantPathfinderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AirbornePollutantPathfinderApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AirbornePollutantPathfinderApplication.class, args);
+    }
 
 }
--- src/test/java/edu/utdallas/cs/app/AirbornePollutantPathfinderApplicationTests.java
+++ src/test/java/edu/utdallas/cs/app/AirbornePollutantPathfinderApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AirbornePollutantPathfinderApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
