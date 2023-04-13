0f148393c7ea4607b4662335bbcff9a32fdac8a8
Arham4 <arhamjsiddiqui@gmail.com>
2023-04-12 23:18:45 -0500

    Replace REST API with GraphQL

--- web/src/main/java/edu/utdallas/cs/app/web/route/RouteController.java
+++ web/src/main/java/edu/utdallas/cs/app/web/route/RouteController.java
@@ -3,32 +3,23 @@ package edu.utdallas.cs.app.web.route;
 import edu.utdallas.cs.app.application.route.RouteService;
 import edu.utdallas.cs.app.domain.route.GeoLocation;
 import edu.utdallas.cs.app.domain.route.Route;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import graphql.kickstart.tools.GraphQLQueryResolver;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/route")
-public class RouteController {
+@Component
+public class RouteController implements GraphQLQueryResolver {
     private final RouteService routeService;
 
     public RouteController(RouteService routeService) {
         this.routeService = routeService;
     }
 
-    @GetMapping
-    public ResponseEntity<List<Route>> getRoute(@RequestParam("originLatitude") double originLatitude,
-                                                @RequestParam("originLongitude") double originLongitude,
-                                                @RequestParam("destinationLatitude") double destinationLatitude,
-                                                @RequestParam("destinationLongitude") double destinationLongitude) {
-        // todo add a verifier for longitude and latitude
+    public List<Route> route(double originLatitude, double originLongitude,
+                             double destinationLatitude, double destinationLongitude) {
         GeoLocation origin = GeoLocation.at(originLatitude, originLongitude);
         GeoLocation destination = GeoLocation.at(destinationLatitude, destinationLongitude);
-        List<Route> route = routeService.getRoutes(origin, destination);
-        return ResponseEntity.ok(route);
+        return routeService.getRoutes(origin, destination);
     }
 }
