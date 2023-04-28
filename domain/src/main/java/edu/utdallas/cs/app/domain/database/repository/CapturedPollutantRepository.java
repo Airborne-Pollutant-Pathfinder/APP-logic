dc04b0cb23b3aea6fa7f9dd0b66eae3371291044
Arham4 <arhamjsiddiqui@gmail.com>
2023-04-28 04:29:08 -0500

    Fix compile errors

--- domain/src/main/java/edu/utdallas/cs/app/domain/database/repository/CapturedPollutantRepository.java
+++ domain/src/main/java/edu/utdallas/cs/app/domain/database/repository/CapturedPollutantRepository.java
@@ -1,7 +1,7 @@
 package edu.utdallas.cs.app.domain.database.repository;
 
+import edu.utdallas.cs.app.domain.database.table.CapturedPollutantTable;
 import edu.utdallas.cs.app.domain.database.table.SensorTable;
-import edu.utdallas.cs.app.domain.table.CapturedPollutantTable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- infrastructure/src/main/java/edu/utdallas/cs/app/infrastructure/sensor/DatabaseCapturedPollutantProvider.java
+++ infrastructure/src/main/java/edu/utdallas/cs/app/infrastructure/sensor/DatabaseCapturedPollutantProvider.java
@@ -2,10 +2,10 @@ package edu.utdallas.cs.app.infrastructure.sensor;
 
 import edu.utdallas.cs.app.domain.database.repository.CapturedPollutantRepository;
 import edu.utdallas.cs.app.domain.database.repository.SensorsRepository;
+import edu.utdallas.cs.app.domain.database.table.CapturedPollutantTable;
 import edu.utdallas.cs.app.domain.database.table.SensorTable;
 import edu.utdallas.cs.app.domain.sensor.CapturedPollutant;
 import edu.utdallas.cs.app.domain.sensor.Sensor;
-import edu.utdallas.cs.app.domain.table.CapturedPollutantTable;
 import jakarta.transaction.Transactional;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
